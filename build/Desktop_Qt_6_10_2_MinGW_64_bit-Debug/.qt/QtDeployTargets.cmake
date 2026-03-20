d89c2ddcf970ee50cf11500eec7e055af9ea080a
StanislavGushchin <GuSt-work@yandex.ru>
2026-03-20 16:01:51 +0300

    re,ove build folder

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Sniffer_FILE C:/Users/gushins/Documents/Sniffer/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/Sniffer.exe)
-set(__QT_DEPLOY_TARGET_Sniffer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Sniffer_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
